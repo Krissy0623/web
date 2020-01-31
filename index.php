2e457c3984f8f4f3f93b87907b6380316e24c2da
Krissy0623 <vulxji0623@gmail.com>
2020-01-31 10:39:20 +0800

    v1.0

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'head.php';
+
+$smarty->assign("a0", "關於我們"); 
+$smarty->assign("a1", "服務項目");
+$smarty->assign("a2", "當季旅遊");
+$smarty->assign("a3", "聯絡我們");   
+
+/*---- 程式結尾-----*/
+$smarty->display('theme.tpl');
